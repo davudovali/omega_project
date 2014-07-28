26a42f236f9a1fff46d6467384bc0ffc2414dd96
davudovali <davudovali@gmail.com>
2014-07-28 23:12:16 +1100

    Set spec file 3

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,23 +1,10 @@
 require 'rails_helper'
 
 describe Expense do
-  before(:all) do
-    @user = User.first || FactoryGirl.create(:user)
-    @expense = FactoryGirl.create(:expense, user: @user)
-    @transaction = FactoryGirl.create(:transaction, expense: @expense)
-  end
-
-
-  it "has user" do
-    expect(@expense.user.id).to be(@user.id)
-  end
-
-  it "has transaction" do
-    expect(@expense.transactions.last.id).to be(@transaction.id)
-  end
+  
 
   it "has a valid factory" do
-    expect(@expense).to be_valid
+    expect(FactoryGirl.build(:expense)).to be_valid
   end
 
 
