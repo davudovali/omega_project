a02763628d65f8caf29a4c87c4e037cd9b7a40d0
umzk <dedo99999@gmail.com>
2014-08-03 00:02:37 +0700

    create CRUD for transactions

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://rubygems.org'
 
 ruby '2.1.1'
+
+gem 'will_paginate'
+
 gem 'rails_12factor'
 
 gem 'rails', '4.1.4'
---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,46 @@
+class TransactionsController < ApplicationController
+  
+  def index 
+    @transactions = Transaction.paginate(per_page: 30, page: params[:page])
+  end
+
+  def new
+    @transaction = Transaction.new
+  end
+
+  def create
+    params.permit!
+    @transaction = Transaction.create(params[:transaction])
+      if @transaction.errors.empty?
+        redirect_to @transaction
+      else
+        render "new"  
+      end
+  end
+  
+  def show
+    @transaction = Transaction.find(params[:id])
+  end
+
+  def edit
+    @transaction = Transaction.find(params[:id])
+  end
+  
+  def update
+    params.permit!
+    @transaction = Transaction.find(params[:id])
+    @transaction.update_attributes(params[:transaction])
+      if @transaction.errors.empty?
+        redirect_to @transaction
+      else
+        render "edit"
+      end
+    end
+
+  def destroy
+    @transaction = Transaction.find(params[:id])
+    @transaction.destroy
+    redirect_to transactions_path
+  end
+
+end
