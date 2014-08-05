d94b221d8a82e6575f12fcb904862162eac6778c
Dmitri <vkosmos26@yandex.ru>
2014-08-05 17:49:30 +0300

    test for show wallets

--- spec/controllers/wallets_controller_spec.rb
+++ spec/controllers/wallets_controller_spec.rb
@@ -1,22 +1,37 @@
-=begin
+
+require 'spec_helper'
 require 'rails_helper'
 
-RSpec.describe WalletsController, :type => :controller do
-
-  describe "GET index" do
-    it "returns http success" do
-      get :index
-      expect(response).to be_success
+describe WalletsController do
+  context "when user not logged in" do
+    describe "GET #index" do
+      it "redirects to login page" do
+        get :index
+        expect(response).to redirect_to new_user_session_path
+      end 
     end
   end
 
-  describe "GET edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to be_success
+  context "when user logged in" do
+    let(:user) { FactoryGirl.create(:user) }
+    subject { FactoryGirl.create(:wallet, user: user)}
+    
+    before do
+      sign_in user
+    end  
+
+    describe "GET #index" do
+      it "render :index view" do
+        get :index
+        expect(response).to render_template :index
+      end
+      
+      it "assigns the requested wallet to subject" do
+        get :index
+        expect(assigns(:wallets)).to eq([subject])
+      end
     end
   end
-
 end
 
-=end
+
