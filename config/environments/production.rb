d80f2d3d166862bfe6b4869867c28d76a746e7ea
davudovali <davudovali@gmail.com>
2014-08-07 21:14:42 +1100

    Do precompile for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
-  config.assets.precompile += %w[application.css bootstrap.min.css]
+  config.assets.precompile += %w[application.css.sass bootstrap.min.css]
 
   # Generate digests for assets URLs.
   config.assets.digest = true
