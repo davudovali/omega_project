a3ac0d362749a056cf8c13764290da1df56a16f3
Dmitri <vkosmos26@yandex.ru>
2014-08-08 15:52:02 +0300

    add validates for Wallet

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -3,7 +3,8 @@ class Wallet < ActiveRecord::Base
 	has_many :transactions, dependent: :destroy
 	has_many :appointments, dependent: :destroy
 	has_many :wallet_transactions, :through => :appointments, dependent: :destroy
-	validates :name, length: {in: 1..30}
+	validates :name, length: {in: 1..30}, presence: true
 	validates :summ, numericality: true
   validates :currency, format: {with: /(USD|RUB|EUR|GBP)/}
+  validates_associated :user
 end
